47b17dcd2d2d821b3eab714d8d13a70514874088
Francesco Sullo <sullof@gmail.com>
2022-08-26 12:09:03 -0700

    Remove unused line in script

--- scripts/deploy-all.js
+++ scripts/deploy-all.js
@@ -7,7 +7,6 @@ let deployUtils;
 
 async function main() {
   deployUtils = new DeployUtils(ethers);
-  const [deployer] = await ethers.getSigners();
   const usdc = await deployUtils.attach("USDCoinMock");
   const usdt = await deployUtils.attach("TetherMock");
   const city = await deployUtils.deployProxy("ByteCity");
